47ec6f231fa1d46821e861d603e3ae3fff7af040
fanchylee <li380157578@sina.cn>
2012-04-05 20:57:34 +0800

    头文件的小改动

---
+++ definations.h
@@ -0,0 +1,9 @@
+#ifndef __DEFINATIONS_H__
+#define __DEFINATIONS_H__
+
+enum portal_option{
+	login,
+	disconnect
+};
+
+#endif
---
+++ globals.h
@@ -0,0 +1,8 @@
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+
+
+extern user[] ;
+extern password[] ;
+
+#endif
