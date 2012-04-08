d369ad7070fc97820e7dcce0bcd36cc390cfd121
fanchylee <li380157578@sina.cn>
2012-04-08 15:51:48 +0800

    大的调整，分离出一个函数

--- portal.c
+++ portal.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <curl/curl.h>
 #include <unistd.h>
 
 
@@ -23,8 +22,7 @@ FILE* debug ;
 FILE* trashfile ;
 	
 
-int perform(int option , FILE* out) ;
-extern char * url_encode(char * ) ;
+extern int perform(enum portal_option option , FILE* out) ;
 extern char * dumpToStrFromFILE(FILE*) ;
 extern int freeDempedStr(char *);
 extern int inforecord(enum portal_option , const char * );
@@ -63,69 +61,7 @@ static int userfileread(const char* userfilename){
 	}
 }
 
-int perform(int option ,FILE* out ){
-	CURL *curl;
-	CURLcode res;
-	char data[1000] = "action=login&url=http\%3A\%2F\%2Fp.nju.edu.cn&login_username=" ;
-	char postdata[] = "&x=29&y=17";
-	char *url ;
-	struct curl_slist *headers=NULL;   
-	
-	curl = curl_easy_init();
-	switch(option){
-		case login:
-		url = "http://p.nju.edu.cn/portal/" ;
-		strcat(data , url_encode(user)) ;
-		strcat(data , "&login_password=") ;
-		strcat(data , url_encode(password)) ;
-		strcat(data , postdata ) ;
-#ifndef RELEASE
-		fputc('\n', debug);
-		fputs(data, debug);
-#endif
-		break ;
-		
-		case disconnect:
-		url = "http://p.nju.edu.cn/portal/index.html" ;
-		strcpy(data , "action=disconnect") ;
-		break ;
-		
-		default :
-		perror("switch error");
-		exit(1) ;
-		break ;
-	}
 
-	if(curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
-#ifdef RELEASE
-		curl_easy_setopt(curl, CURLOPT_VERBOSE , 0) ;
-#endif
-#ifndef  RELEASE
-//		curl_easy_setopt(curl, CURLOPT_VERBOSE , 1) ;
-#endif
-#ifdef RELEASE
-//		curl_easy_setopt(curl, CURLOPT_NOPROGRESS , 0) ;
-#endif
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data); 
-
-		headers = curl_slist_append(headers, "Host: p.nju.edu.cn");
-		headers = curl_slist_append(headers, "Connection: keep-alive");
-		headers = curl_slist_append(headers, "Referer: http://p.nju.edu.cn/portal/");
-		headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-	
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
-
-		res = curl_easy_perform(curl);
-		
-
-		/* always cleanup */
-		curl_slist_free_all(headers); /* free custom header list */
-		curl_easy_cleanup(curl);
-	}
-	return 0;
-}
 int main(int argc, char *argv[]){
 	char* option = NULL ;
 	
